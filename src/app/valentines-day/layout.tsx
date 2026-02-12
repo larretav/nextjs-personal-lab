3ea5203b2b15ad23b3402373480f0ef5e0bef756
Alejandro Larreta Valenzuela <larreta_vzla@hotmail.com>
2026-02-12 16:08:24 -0700

    Inicio de la contruccion de la pagina de San Valentin

---
+++ src/app/valentines-day/layout.tsx
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'Valentines Day',
+  description: 'Valentines Day',
+}
+
+export default function ValentinesDayLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,11 +9,10 @@ import { ThemeSwitch } from "@/src/components/theme-switch";
 export const Navbar = () => {
 
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky" >
-      <NavbarContent className="outline" justify="end">
-        <div>
-          <ThemeSwitch />
-        </div>
+    <HeroUINavbar  maxWidth="xl" position="sticky" className="bg-red-300">
+      
+      <NavbarContent justify="end">
+        <ThemeSwitch />
       </NavbarContent>
 
     </HeroUINavbar>
